03aca79cecd211010aff9f3bc49f28156b1f51b4
Kamran <kamranmuazzam@gmail.com>
2024-05-07 02:10:40 +0600

    update

--- Posts.jsx
+++ Posts.jsx
@@ -1,65 +1,51 @@
-import "./post.scss";
-import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
-import FavoriteOutlinedIcon from "@mui/icons-material/FavoriteOutlined";
-import TextsmsOutlinedIcon from "@mui/icons-material/TextsmsOutlined";
-import ShareOutlinedIcon from "@mui/icons-material/ShareOutlined";
-import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
-import { Link } from "react-router-dom";
-import Comments from "../comments/Comments";
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
+import Post from "../post/Post";
+import "./posts.scss";
 
-const Post = ({ post }) => {
-  const [commentOpen, setCommentOpen] = useState(false);
-  const [liked, setLiked] = useState(false);
-  const [likesCount, setLikesCount] = useState(12); // Initial likes count
+const Posts = (props) => {
+  const [posts, setPosts] = useState([]);
 
-  const handleLikeClick = () => {
-    // Toggle the liked state
-    setLiked((prevLiked) => !prevLiked);
-    // Update likes count based on current liked state
-    setLikesCount((prevCount) => (liked ? prevCount - 1 : prevCount + 1));
-  };
+  useEffect(() => {
+    const pp = [
+      {
+        id: 1,
+        name: "Anik",
+        userId: 1,
+        profilePic:
+          "https://i.ibb.co/8YkTy5X/399802121-2096661420678789-8201301384481582908-n.jpg",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+        img: "https://i.ibb.co/8YkTy5X/399802121-2096661420678789-8201301384481582908-n.jpg?auto=compress&cs=tinysrgb&w=1600",
+      },
+      {
+        id: 2,
+        name: "Eesor",
+        userId: 1,
+        profilePic:
+          "https://i.ibb.co/ggyKMj3/279725233-1949964898521765-5705253541412336116-n.jpg",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+        img: "https://i.ibb.co/ggyKMj3/279725233-1949964898521765-5705253541412336116-n.jpg?auto=compress&cs=tinysrgb&w=1600",
+      },
+      {
+        id: 3,
+        name: "Anik",
+        userId: 2,
+        profilePic:
+          "https://i.ibb.co/8YkTy5X/399802121-2096661420678789-8201301384481582908-n.jpg?auto=compress&cs=tinysrgb&w=1600",
+        desc:
+          "Tenetur iste voluptates dolorem rem commodi voluptate pariatur, voluptatum, laboriosam consequatur enim nostrum cumque! Maiores a nam non adipisci minima modi tempore.",
+      },
+    ];
+    setPosts(pp);
+    props.setPosts(pp);
+  }, []); // Empty dependency array ensures this effect runs only once on mount
 
   return (
-    <div className="post">
-      <div className="container">
-        <div className="user">
-          <div className="userInfo">
-            <img src={post.profilePic} alt="" />
-            <div className="details">
-              <Link
-                to={`/profile/${post.userId}`}
-                style={{ textDecoration: "none", color: "inherit" }}
-              >
-                <span className="name">{post.name}</span>
-              </Link>
-              <span className="date">1 min ago</span>
-            </div>
-          </div>
-          <MoreHorizIcon />
-        </div>
-        <div className="content">
-          <p>{post.desc}</p>
-          <img src={post.img} alt="" />
-        </div>
-        <div className="info">
-          <div className="item" onClick={handleLikeClick}>
-            {liked ? <FavoriteOutlinedIcon /> : <FavoriteBorderOutlinedIcon />}
-            {likesCount} Likes
-          </div>
-          <div className="item" onClick={() => setCommentOpen(!commentOpen)}>
-            <TextsmsOutlinedIcon />
-            12 Comments
-          </div>
-          <div className="item">
-            <ShareOutlinedIcon />
-            Share
-          </div>
-        </div>
-        {commentOpen && <Comments />}
-      </div>
+    <div className="posts">
+      {posts.map((post) => (
+        <Post post={post} key={post.id} />
+      ))}
     </div>
   );
 };
 
-export default Post;
+export default Posts;
